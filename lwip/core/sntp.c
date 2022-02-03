6a6f43686b572416c9c5775aa8ca8f32fab656f5
Kristoffer Ek <stoffer@skulp.net>
2022-02-03 20:46:23 +0100

    reverted last commit
    didn't work

--- lwip/core/sntp.c
+++ lwip/core/sntp.c
@@ -218,10 +218,8 @@
 #define SNTP_OFFSET_RECEIVE_TIME    32
 #define SNTP_OFFSET_TRANSMIT_TIME   40
 
-/* number of seconds between 1900 and 1970 (MSB=1)*/
+/* number of seconds between 1900 and 1970 */
 #define DIFF_SEC_1900_1970         (2208988800UL)
-/* number of seconds between 1970 and Feb 7, 2036 (6:28:16 UTC) (MSB=0) */
-#define DIFF_SEC_1970_2036         (2085978496UL)
 
 /**
  * SNTP packet format (without optional fields)
@@ -664,18 +662,15 @@ static void ICACHE_FLASH_ATTR
 sntp_process(u32_t *receive_timestamp)
 {
   /* convert SNTP time (1900-based) to unix GMT time (1970-based)
-   * if MSB is 0, SNTP time is 2036-based!
+   * @todo: if MSB is 1, SNTP time is 2036-based!
    */
-  u32_t rx_secs = ntohl(receive_timestamp[0]);
-  int is_1900_based = ((rx_secs & 0x80000000) != 0);
-  u32_t t = is_1900_based ? (rx_secs - DIFF_SEC_1900_1970) : (rx_secs + DIFF_SEC_1970_2036);
-  time_t tim = t;
+  time_t t = (ntohl(receive_timestamp[0]) - DIFF_SEC_1900_1970);
 
 #if SNTP_CALC_TIME_US
   u32_t us = ntohl(receive_timestamp[1]) / 4295;
   SNTP_SET_SYSTEM_TIME_US(t, us);
   /* display local time from GMT time */
-  LWIP_DEBUGF(SNTP_DEBUG_TRACE, ("sntp_process: %s, %"U32_F" us", ctime(&tim), us));
+  LWIP_DEBUGF(SNTP_DEBUG_TRACE, ("sntp_process: %s, %"U32_F" us", ctime(&t), us));
 
 #else /* SNTP_CALC_TIME_US */
 
